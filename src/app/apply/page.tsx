24923b66a0a71f9ef1382eb621c5d3a48d9444b1
“Mo <mo.zaqout@gmail.com>
2025-06-12 15:21:31 +0200

    created stepper

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,14 +1,10 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 export default function ApplyPage() {
-  const searchParams = useSearchParams();
-  const von = searchParams.get("von");
-  const nach = searchParams.get("nach");
-
-  // steps data
-  // forms
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = [{ title: "Auszugsort" }, { title: "Einzugsort" }, { title: "Termin" }];
 
   return (
     <div className={styles.page}>
@@ -18,18 +14,57 @@ export default function ApplyPage() {
         - Einzugsort [Einzugsort, Einzug in, Aufzug & Laufweg, Zusatzleistungen für Ihren Einzug]
         Einreichen step
       */}
-      {von && nach ? <div className='alert-info'>Ales gut!</div> : ""}
+      <div className={styles.stepper}>
+        {/* step icons */}
+        <div className={styles.header}>
+          {steps.map((a, i) => (
+            <div key={i} className='flex'>
+              <div className={styles.headerItem}>
+                <span className={`${styles["stepNumber"]}  ${activeStep == i && styles["activeStepNumber"]}`}>{i + 1}</span>
+                <p className={`${styles["stepTitle"]}  ${activeStep == i && styles["activeStepTitle"]}`}>{a.title}</p>
+              </div>
+              {steps.length == i + 1 ? "" : <div className={styles.separator} />}
+            </div>
+          ))}
+        </div>
 
-      <div className={styles.header}>
-        <p>{von}</p>
-        <p>-</p>
-        <p>{nach}</p>
+        {/*  */}
+        {/* step content */}
+        <div className={styles.stepsContainer}>
+          <div className={`${styles["step"]}  ${activeStep == 0 && styles["activeStep"]}`}>
+            <h1>Auszugsort</h1>
+          </div>
+          <div className={`${styles["step"]}  ${activeStep == 1 && styles["activeStep"]}`}>
+            <h1>Einzugsort</h1>
+          </div>
+          <div className={`${styles["step"]}  ${activeStep == 2 && styles["activeStep"]}`}>
+            <h1>Termin</h1>
+          </div>
+          <div className={styles.stepsButtons}>
+            <button
+              className='notFilledBtn'
+              disabled={activeStep == 0}
+              onClick={() => {
+                if (activeStep == 0) return;
+                setActiveStep(activeStep - 1);
+              }}
+            >
+              zurück
+            </button>
+            <button
+              disabled={activeStep == steps.length - 1}
+              onClick={() => {
+                if (activeStep == steps.length - 1) return;
+                setActiveStep(activeStep + 1);
+              }}
+            >
+              Weiter
+            </button>
+          </div>
+        </div>
       </div>
-
-      <div className={styles.steps}>
-        steps here
-        {/* 1st Step, Auszugsort */}
-      </div>
+      {/* stepper ends */}
+      {/*  */}
     </div>
   );
 }
